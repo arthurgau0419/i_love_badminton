a1fcc6dbb08f304eee043ada04f1962492f3c9cc
arthurkao <arthurkao@pklotcorp.com>
2021-08-21 09:46:32 +0800

    Add support for multiple times, pid.
    wip
    
    
    wip

--- badminton/spiders/spider.py
+++ badminton/spiders/spider.py
@@ -2,6 +2,7 @@ import scrapy
 import re
 from img import convert_image, change_image_to_text, get_image
 import time
+import datetime
 
 class BadmintonSpider(scrapy.Spider):
 
@@ -14,8 +15,12 @@ class BadmintonSpider(scrapy.Spider):
     tp = None
     pt = None
     pid = None
+    fire_datetime = None
+    srv_datetime = None
 
-    def __init__(self, account, password, date, order_time, tp, pt, pid, **kwargs):
+    tz = datetime.timezone(datetime.timedelta(hours=+8))
+
+    def __init__(self, account, password, fire_date, date, order_time, tp, pt, pid, **kwargs):
         self.account = account
         self.password = password
         self.date = date
@@ -23,6 +28,11 @@ class BadmintonSpider(scrapy.Spider):
         self.tp = tp
         self.pt = pt
         self.pid = pid
+
+        self.fire_datetime = datetime.datetime.strptime(fire_date, '%Y/%m/%d')\
+            .replace(tzinfo=self.tz)\
+            .astimezone(datetime.timezone.utc)
+
         super().__init__(**kwargs)
 
     def start_requests(self):
@@ -49,6 +59,7 @@ class BadmintonSpider(scrapy.Spider):
         yield self.captcha_request(response, captcha_url)
 
     def login(self, response):
+        self.update_srv_datetime(response)
         login_response = response.meta.get('login_response')
         captcha_url = response.meta.get('captcha_url')
         captcha_data = response.body
@@ -62,6 +73,8 @@ class BadmintonSpider(scrapy.Spider):
         text = next(it, None)
         if text:
             self.log("驗證碼為: {}".format(text))
+            time_list = self.order_time.split(',')
+            pid_list = self.pid.split(',')
             yield scrapy.FormRequest(
                 login_response.urljoin('tp{}.aspx?module=login_page&files=login'.format(self.tp)),
                 method='POST',
@@ -76,16 +89,20 @@ class BadmintonSpider(scrapy.Spider):
                     'login_response': login_response,
                     'captcha_url': captcha_url
                 },
+                cb_kwargs={
+                    'time_list': time_list,
+                    'pid_list': pid_list
+                }
             )
         else:
             yield self.captcha_request(login_response, captcha_url)
 
-    def order_request(self, response):
+    def order_request(self, response, order_time, pid):
 
         path = 'tp{}.aspx?module=net_booking&files=booking_place&StepFlag=25&QPid={}&QTime={}&PT={}&D={}'.format(
             self.tp,
-            self.pid,
-            self.order_time,
+            pid,
+            order_time,
             self.pt,
             self.date
         )
@@ -95,9 +112,20 @@ class BadmintonSpider(scrapy.Spider):
             callback=self.parse,
             errback=self.do_order,
             dont_filter=True,
+            cb_kwargs={
+                'time_list': [order_time],
+                'pid_list': [pid]
+            }
         )
 
-    def do_order(self, response):
+    def do_order(self, response, *args, **kwargs):
+        now = datetime.datetime.now(tz=self.tz) \
+            .astimezone(datetime.timezone.utc)
+        seconds_to_fire = (self.fire_datetime - now).total_seconds()
+        if seconds_to_fire < -10:
+            print('已超過十秒，結束')
+            return
+
         login_response = response.meta.get('login_response')
         captcha_url = response.meta.get('captcha_url')
         # 去看他們前端真的就這樣趴
@@ -112,9 +140,16 @@ class BadmintonSpider(scrapy.Spider):
         elif result == '3':
             yield self.captcha_request(login_response, captcha_url)
         else:
-            yield self.order_request(response)
+            if seconds_to_fire > 3:
+                print('還剩: {} 秒開始，等待中...'.format(seconds_to_fire))
+                time.sleep(seconds_to_fire - 3)
+            time_list = kwargs.get('time_list', [])
+            pid_list = kwargs.get('pid_list', [])
+            for pid in pid_list:
+                for order_time in time_list:
+                    yield self.order_request(response, order_time=order_time, pid=pid)
 
-    def parse(self, response):
+    def parse(self, response, *args, **kwargs):
         content = response.body.decode('utf8')
         pattern = "tp{}\.aspx\?module=net_booking&files=booking_place&X=(\d+)&Y=(\d+)&StepFlag=3".format(self.tp)
         matchs = re.findall(pattern, content)
@@ -122,14 +157,16 @@ class BadmintonSpider(scrapy.Spider):
         if match:
             result = match[0]
             if result != '1':
-                delay = 1
-                self.log("預定失敗，({})秒後再試...".format(delay))
-                # 客氣一點
-                # time.sleep(delay)
-                yield self.order_request(response)
+                time_list = kwargs.get('time_list', [])
+                pid_list = kwargs.get('pid_list', [])
+                for order_time in time_list:
+                    for pid in pid_list:
+                        yield self.order_request(response, order_time=order_time, pid=pid)
             else:
                 order = match[1]
-                self.log("預定成功(日期:{} 時段:{} 場地:{})，訂單號碼為: {}".format(self.date, self.order_time, self.pid, order))
+                # todo: parse time, pid
+                text = "預定成功(日期:{} 時段:{} 場地:{})，訂單號碼為: {}".format(self.date, self.order_time, self.pid, order)
+                yield {'success': text}
         else:
             # 代表需要重新登入
             yield scrapy.Request(
@@ -137,3 +174,10 @@ class BadmintonSpider(scrapy.Spider):
                 callback=self.parse_login,
                 dont_filter=True
             )
+
+    def update_srv_datetime(self, response):
+        date = response.headers.get('Date')
+        if date:
+            self.srv_datetime = datetime.datetime \
+                .strptime(date.decode("utf-8"), '%a, %d %b %Y %H:%M:%S %Z') \
+                .replace(tzinfo=datetime.timezone.utc)
--- begin.py
+++ begin.py
@@ -1,5 +1,19 @@
 from scrapy import cmdline
+import os
 
 # you can run this script using IDE.
 # cmdline.execute("scrapy crawl badminton".split())
-cmdline.execute("scrapy crawl badminton -a account=ooo -a password=xxx -a date=2019/10/02 -a order_time=20 -a pid=87".split())
+cmdline.execute(
+    "scrapy crawl badminton -a account={} -a password={} -a fire_date={} -a date={} -a order_time={}  -a tp={} -a pt={} -a pid={}"
+        .format(
+            os.environ.get('account'),
+            os.environ.get('password'),
+            os.environ.get('fire_date'),
+            os.environ.get('date'),
+            os.environ.get('order_time'),
+            os.environ.get('tp'),
+            os.environ.get('pt'),
+            os.environ.get('pid'),
+    )
+        .split()
+)
